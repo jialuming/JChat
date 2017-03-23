606eae1078d8af0dff9b1c2cd68e213c381528e7
xiaoj <xiaoj@DESKTOP-JLKB0CD>
2017-03-24 00:18:14 +0800

    new

---
+++ JDBService/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JDBService
+{
+    public class Class1
+    {
+    }
+}
