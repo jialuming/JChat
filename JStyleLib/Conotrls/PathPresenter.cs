e1493b1a3712bcd7e74781c81b0940dea8d8db4a
jialuming <xiaojia001100@126.com>
2017-04-18 18:09:04 +0800

    1

--- JStyleLib/Conotrls/PathPresenter.cs
+++ JStyleLib/Conotrls/PathPresenter.cs
@@ -13,8 +13,9 @@ namespace JStyleLib.Conotrls
 {
     public class PathPresenter : ContentPresenter
     {
-        private Path path = null;
-        private Brush brushPool;
+        List<Path> pathList = new List<Path>();
+        private Brush fillPool;
+        private Brush strokePool;
 
         public Brush Fill
         {
@@ -46,52 +47,82 @@ namespace JStyleLib.Conotrls
         public static readonly DependencyProperty StrokeThicknessProperty =
             DependencyProperty.Register("StrokeThickness", typeof(double), typeof(PathPresenter), new PropertyMetadata((double)1, StrokeThicknessChanged));
 
+
         private static void FillChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             PathPresenter pc = d as PathPresenter;
-            if (pc.path == null)
-                pc.path = ControlHelper.FindVisualChild<Path>(pc);
-            if (pc.path != null)
-                if (pc.path != null)
+
+            if (pc.pathList == null || pc.pathList.Count == 0)
+            {
+                DependencyObject deo = pc.Content as DependencyObject;
+                if (deo != null)
+                    pc.pathList = ControlHelper.GetChildObjects<Path>(deo);
+            }
+            if (pc.pathList != null && pc.pathList.Count > 0)
+            {
+                if (e.NewValue == null)
                 {
-                    if (e.NewValue == null)
+                    foreach (Path path in pc.pathList)
                     {
-                        pc.path.Fill = pc.brushPool;
-                    }
-                    else
-                    {
-                        pc.path.Fill = e.NewValue as Brush;
-                        pc.brushPool = e.OldValue == null ? null : (Brush)e.OldValue;
+                        path.Fill = pc.fillPool;
                     }
                 }
+                else
+                {
+                    foreach (Path path in pc.pathList)
+                    {
+                        pc.fillPool = pc.fillPool ?? path.Fill;
+                        path.Fill = e.NewValue as Brush;
+                    }
+                }
+            }
         }
 
         private static void StrokeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             PathPresenter pc = d as PathPresenter;
-            if (pc.path == null)
-                pc.path = ControlHelper.FindVisualChild<Path>(pc);
-            if (pc.path != null)
+            if (pc.pathList == null || pc.pathList.Count == 0)
             {
+                DependencyObject deo = pc.Content as DependencyObject;
+                if (deo != null)
+                    pc.pathList = ControlHelper.GetChildObjects<Path>(deo);
+            }
+            if (pc.pathList != null && pc.pathList.Count > 0)
+            {
+
                 if (e.NewValue == null)
                 {
-                    pc.path.Stroke = pc.brushPool;
+                    foreach (Path path in pc.pathList)
+                    {
+                        path.Fill = pc.strokePool;
+                    }
                 }
                 else
                 {
-                    pc.path.Stroke = e.NewValue as Brush;
-                    pc.brushPool = e.OldValue == null ? null : (Brush)e.OldValue;
+                    foreach (Path path in pc.pathList)
+                    {
+                        pc.strokePool = pc.strokePool ?? path.Stroke;
+                        path.Stroke = e.NewValue as Brush;
+                    }
                 }
             }
         }
 
+
         private static void StrokeThicknessChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             PathPresenter pc = d as PathPresenter;
-            if (pc.path == null)
-                pc.path = ControlHelper.FindVisualChild<Path>(pc);
-            if (pc.path != null)
-                pc.path.StrokeThickness = (double)e.NewValue;
+            if (pc.pathList == null || pc.pathList.Count == 0)
+            {
+                DependencyObject deo = pc.Content as DependencyObject;
+                if (deo != null)
+                    pc.pathList = ControlHelper.GetChildObjects<Path>(deo);
+            }
+            if (pc.pathList != null && pc.pathList.Count > 0)
+                foreach (Path path in pc.pathList)
+                {
+                    path.StrokeThickness = (double)e.NewValue;
+                }
         }
     }
 }
--- JToolsLib/DataHelper/ControlHelper.cs
+++ JToolsLib/DataHelper/ControlHelper.cs
@@ -18,6 +18,7 @@ namespace JToolsLib.DataHelper
         /// <returns></returns>
         public static T FindParent<T>(DependencyObject i_dp) where T : DependencyObject
         {
+            if (i_dp == null) throw new ArgumentNullException();
             DependencyObject dobj = (DependencyObject)VisualTreeHelper.GetParent(i_dp);
             if (dobj != null)
             {
@@ -48,6 +49,8 @@ namespace JToolsLib.DataHelper
         public static childItem FindVisualChild<childItem>(DependencyObject obj)
     where childItem : DependencyObject
         {
+            if (obj == null) throw new ArgumentNullException();
+
             for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
             {
                 DependencyObject child = VisualTreeHelper.GetChild(obj, i);
@@ -62,7 +65,30 @@ namespace JToolsLib.DataHelper
             }
             return null;
         }
+        /// <summary>  
+        /// 获得指定元素的所有子元素  
+        /// </summary>  
+        /// <typeparam name="T"></typeparam>  
+        /// <param name="obj"></param>  
+        /// <returns></returns>  
+        public static List<T> GetChildObjects<T>(DependencyObject obj) where T : DependencyObject
+        {
+            if (obj == null) throw new ArgumentNullException();
+            DependencyObject child = null;
+            List<T> childList = new List<T>();
 
+            for (int i = 0; i <= VisualTreeHelper.GetChildrenCount(obj) - 1; i++)
+            {
+                child = VisualTreeHelper.GetChild(obj, i);
+
+                if (child is T)
+                {
+                    childList.Add((T)child);
+                }
+                childList.AddRange(GetChildObjects<T>(child));
+            }
+            return childList;
+        }
 
     }
 }
