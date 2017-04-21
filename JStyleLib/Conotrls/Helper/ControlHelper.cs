f8b36af9d335eb7d00943c307cab2218cda23289
jialuming <xiaojia001100@126.com>
2017-04-21 17:55:37 +0800

    1

--- JStyleLib/Conotrls/Helper/ControlHelper.cs
+++ JStyleLib/Conotrls/Helper/ControlHelper.cs
@@ -18,8 +18,17 @@ namespace JStyleLib.Conotrls.Helper
             DependencyProperty.RegisterAttached("HeaderBackground", typeof(Brush), typeof(ControlHelper), new PropertyMetadata(null));
         public static readonly DependencyProperty HeaderMarkBackgroundProperty =
             DependencyProperty.RegisterAttached("HeaderMarkBackground", typeof(Brush), typeof(ControlHelper), new PropertyMetadata(null));
-      public static readonly DependencyProperty HeaderIconContentProperty =
-            DependencyProperty.RegisterAttached("HeaderIconContent", typeof(object), typeof(ControlHelper), new PropertyMetadata(null));
+        public static readonly DependencyProperty HeaderIconContentProperty =
+              DependencyProperty.RegisterAttached("HeaderIconContent", typeof(object), typeof(ControlHelper), new PropertyMetadata(null));
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.RegisterAttached("CornerRadius", typeof(CornerRadius), typeof(ControlHelper), new PropertyMetadata(new CornerRadius()));
+        public static readonly DependencyProperty MarkBackgroundProperty =
+            DependencyProperty.RegisterAttached("MarkBackground", typeof(Brush), typeof(ControlHelper), new PropertyMetadata(null));
+        public static readonly DependencyProperty PopupBackgroundProperty =
+            DependencyProperty.RegisterAttached("PopupBackground", typeof(Brush), typeof(ControlHelper), new PropertyMetadata(null));
+        public static readonly DependencyProperty IconWidthProperty =
+            DependencyProperty.RegisterAttached("IconWidth", typeof(GridLength), typeof(ControlHelper), new PropertyMetadata(new GridLength(1, GridUnitType.Auto)));
+
 
         [AttachedPropertyBrowsableForType(typeof(TreeViewItem))]
         public static object GetAttachContent(DependencyObject obj)
@@ -43,6 +52,7 @@ namespace JStyleLib.Conotrls.Helper
         {
             obj.SetValue(HeaderBackgroundProperty, value);
         }
+
         [AttachedPropertyBrowsableForType(typeof(MenuItem))]
         [AttachedPropertyBrowsableForType(typeof(TreeViewItem))]
         public static Brush GetHeaderMarkBackground(DependencyObject obj)
@@ -65,6 +75,50 @@ namespace JStyleLib.Conotrls.Helper
         public static void SetHeaderIconContent(DependencyObject obj, object value)
         {
             obj.SetValue(HeaderIconContentProperty, value);
+        }
+
+        [AttachedPropertyBrowsableForType(typeof(MenuItem))]
+        public static CornerRadius GetCornerRadius(DependencyObject obj)
+        {
+            return (CornerRadius)obj.GetValue(CornerRadiusProperty);
+        }
+
+        public static void SetCornerRadius(DependencyObject obj, CornerRadius value)
+        {
+            obj.SetValue(CornerRadiusProperty, value);
+        }
+
+        [AttachedPropertyBrowsableForType(typeof(MenuItem))]
+        public static Brush GetMarkBackground(DependencyObject obj)
+        {
+            return (Brush)obj.GetValue(MarkBackgroundProperty);
+        }
+
+        public static void SetMarkBackground(DependencyObject obj, Brush value)
+        {
+            obj.SetValue(MarkBackgroundProperty, value);
+        }
+
+        [AttachedPropertyBrowsableForType(typeof(MenuItem))]
+        public static Brush GetPopupBackground(DependencyObject obj)
+        {
+            return (Brush)obj.GetValue(PopupBackgroundProperty);
+        }
+
+        public static void SetPopupBackground(DependencyObject obj, Brush value)
+        {
+            obj.SetValue(PopupBackgroundProperty, value);
+        }
+
+        [AttachedPropertyBrowsableForType(typeof(MenuItem))]
+        public static GridLength GetIconWidth(DependencyObject obj)
+        {
+            return (GridLength)obj.GetValue(IconWidthProperty);
+        }
+
+        public static void SetIconWidth(DependencyObject obj, GridLength value)
+        {
+            obj.SetValue(IconWidthProperty, value);
         }
     }
 }
