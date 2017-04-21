41c69e06f3e70f0270ca133eab91b525e709a35f
jialuming <xiaojia001100@126.com>
2017-04-21 15:32:00 +0800

    1

---
+++ JStyleLib/Conotrls/Helper/ControlHelper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+
+namespace JStyleLib.Conotrls.Helper
+{
+    public class ControlHelper
+    {
+        public static readonly DependencyProperty AttachContentProperty =
+          DependencyProperty.RegisterAttached("AttachContent", typeof(object), typeof(ControlHelper), new PropertyMetadata(null));
+        public static readonly DependencyProperty HeaderBackgroundProperty =
+            DependencyProperty.RegisterAttached("HeaderBackground", typeof(Brush), typeof(ControlHelper), new PropertyMetadata(null));
+        public static readonly DependencyProperty HeaderMarkBackgroundProperty =
+            DependencyProperty.RegisterAttached("HeaderMarkBackground", typeof(Brush), typeof(ControlHelper), new PropertyMetadata(null));
+      public static readonly DependencyProperty HeaderIconContentProperty =
+            DependencyProperty.RegisterAttached("HeaderIconContent", typeof(object), typeof(ControlHelper), new PropertyMetadata(null));
+
+        [AttachedPropertyBrowsableForType(typeof(TreeViewItem))]
+        public static object GetAttachContent(DependencyObject obj)
+        {
+            return (object)obj.GetValue(AttachContentProperty);
+        }
+
+        public static void SetAttachContent(DependencyObject obj, object value)
+        {
+            obj.SetValue(AttachContentProperty, value);
+        }
+
+        [AttachedPropertyBrowsableForType(typeof(MenuItem))]
+        [AttachedPropertyBrowsableForType(typeof(TreeViewItem))]
+        public static Brush GetHeaderBackground(DependencyObject obj)
+        {
+            return (Brush)obj.GetValue(HeaderBackgroundProperty);
+        }
+
+        public static void SetHeaderBackground(DependencyObject obj, Brush value)
+        {
+            obj.SetValue(HeaderBackgroundProperty, value);
+        }
+        [AttachedPropertyBrowsableForType(typeof(MenuItem))]
+        [AttachedPropertyBrowsableForType(typeof(TreeViewItem))]
+        public static Brush GetHeaderMarkBackground(DependencyObject obj)
+        {
+            return (Brush)obj.GetValue(HeaderMarkBackgroundProperty);
+        }
+
+        public static void SetHeaderMarkBackground(DependencyObject obj, Brush value)
+        {
+            obj.SetValue(HeaderMarkBackgroundProperty, value);
+        }
+
+        [AttachedPropertyBrowsableForType(typeof(MenuItem))]
+        [AttachedPropertyBrowsableForType(typeof(TreeViewItem))]
+        public static object GetHeaderIconContent(DependencyObject obj)
+        {
+            return (object)obj.GetValue(HeaderIconContentProperty);
+        }
+
+        public static void SetHeaderIconContent(DependencyObject obj, object value)
+        {
+            obj.SetValue(HeaderIconContentProperty, value);
+        }
+    }
+}
--- JStyleLib/Conotrls/Helper/TreeItemHelper.cs
+++ JStyleLib/Conotrls/Helper/TreeItemHelper.cs
@@ -10,46 +10,8 @@ namespace JStyleLib.Conotrls.Helper
 {
     public class TreeItemHelper
     {
-        public static readonly DependencyProperty AttachContentProperty =
-            DependencyProperty.RegisterAttached("AttachContent", typeof(object), typeof(TreeItemHelper), new PropertyMetadata(null));
-        public static readonly DependencyProperty HeaderBackgroundProperty =
-            DependencyProperty.RegisterAttached("HeaderBackground", typeof(Brush), typeof(TreeItemHelper), new PropertyMetadata(null));
-        public static readonly DependencyProperty HeaderMarkBackgroundProperty =
-            DependencyProperty.RegisterAttached("HeaderMarkBackground", typeof(Brush), typeof(TreeItemHelper), new PropertyMetadata(null));
         public static readonly DependencyProperty HeaderHeightProperty =
             DependencyProperty.RegisterAttached("HeaderHeight", typeof(GridLength), typeof(TreeItemHelper), new PropertyMetadata(new GridLength(1, GridUnitType.Auto)));
-        public static readonly DependencyProperty HeaderIconContentProperty =
-            DependencyProperty.RegisterAttached("HeaderIconContent", typeof(object), typeof(TreeItemHelper), new PropertyMetadata(null));
-
-        public static object GetAttachContent(DependencyObject obj)
-        {
-            return (object)obj.GetValue(AttachContentProperty);
-        }
-
-        public static void SetAttachContent(DependencyObject obj, object value)
-        {
-            obj.SetValue(AttachContentProperty, value);
-        }
-
-        public static Brush GetHeaderBackground(DependencyObject obj)
-        {
-            return (Brush)obj.GetValue(HeaderBackgroundProperty);
-        }
-
-        public static void SetHeaderBackground(DependencyObject obj, Brush value)
-        {
-            obj.SetValue(HeaderBackgroundProperty, value);
-        }
-
-        public static Brush GetHeaderMarkBackground(DependencyObject obj)
-        {
-            return (Brush)obj.GetValue(HeaderMarkBackgroundProperty);
-        }
-
-        public static void SetHeaderMarkBackground(DependencyObject obj, Brush value)
-        {
-            obj.SetValue(HeaderMarkBackgroundProperty, value);
-        }
 
         public static GridLength GetHeaderHeight(DependencyObject obj)
         {
@@ -60,16 +22,5 @@ namespace JStyleLib.Conotrls.Helper
         {
             obj.SetValue(HeaderHeightProperty, value);
         }
-
-        public static object GetHeaderIconContent(DependencyObject obj)
-        {
-            return (object)obj.GetValue(HeaderIconContentProperty);
-        }
-
-        public static void SetHeaderIconContent(DependencyObject obj, object value)
-        {
-            obj.SetValue(HeaderIconContentProperty, value);
-        }
-
     }
 }
